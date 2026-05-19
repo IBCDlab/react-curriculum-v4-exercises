6412d8842caf436429f89e5ada3604b59a50d365
Ina Bryzheuskaya <inabri783@gmail.com>
2026-05-19 00:20:44 -0500

    Create components for fetching and displaying posts

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,11 +1,29 @@
 import './Lesson07Styles.css';
+import { getPosts } from './api.js';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function FetchOnRender() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function loadPosts() {
+      const data = await getPosts();
+      setPosts(data);
+    }
+    loadPosts();
+  }, []);
+
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
-        TODO: Replace me with fetched data when the component renders
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
